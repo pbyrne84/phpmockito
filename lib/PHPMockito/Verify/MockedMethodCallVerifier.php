4e09ea95de9604cc85b8495286a72b50ab3b4309
Patrick Bryne <p.bryne@sthree.com>
2013-08-23 20:56:39 +0100

    add messages to verify

--- lib/PHPMockito/Verify/MockedMethodCallVerifier.php
+++ lib/PHPMockito/Verify/MockedMethodCallVerifier.php
@@ -6,7 +6,7 @@ use PHPMockito\Action\ExpectedMethodCall;
 use PHPMockito\Action\MethodCall;
 use PHPMockito\CallMatching\CallMatcher;
 
-class MockedMethodCallVerifier implements MockedMethodCallLogger, VerificationTester{
+class MockedMethodCallVerifier implements MockedMethodCallLogger, VerificationTester {
     const CLASS_NAME = __CLASS__;
 
     /** @var ExpectedMethodCall[] */
@@ -30,7 +30,7 @@ class MockedMethodCallVerifier implements MockedMethodCallLogger, VerificationTe
     public function logMethodCall( MethodCall $methodCall ) {
         if ( $methodCall instanceof DebugBackTraceMethodCall ) {
             $this->actualMethodCallList[ ] = $methodCall->castToMethodCall();
-        }else{
+        } else {
             $this->actualMethodCallList[ ] = $methodCall;
         }
     }
@@ -43,23 +43,61 @@ class MockedMethodCallVerifier implements MockedMethodCallLogger, VerificationTe
      * @throws \PHPUnit_Framework_AssertionFailedError - if actual call count is not not equal to expected
      */
     public function assertCallCount( MethodCall $expectedMethodCall, $expectedCallCount ) {
-        $actualCallCount = 0;
+        $actualCallCount   = 0;
+        $actualCallMessage = '';
         foreach ( $this->actualMethodCallList as $actualMethodCall ) {
-            if ( $this->callMatcher->matchCall( $actualMethodCall, $expectedMethodCall )) {
+            $actualCallMessage .= $this->generateMessage( $actualMethodCall ) . "\n";
+            if ( $this->callMatcher->matchCall( $actualMethodCall, $expectedMethodCall ) ) {
                 $actualCallCount++;
             }
         }
 
-        if ( $actualCallCount != $expectedCallCount ) {
-            $this->raiseAssertionError(
-                'Expected a call of ' . $expectedCallCount . ' got ' . $actualCallCount,
-                $expectedMethodCall
-            );
+        if ( $actualCallCount == $expectedCallCount ) {
+            return;
         }
+
+
+        $expectedMessage =
+                'Expected a call of count ' . $expectedCallCount . ' got ' . $actualCallCount . "\n" .
+                $this->generateHeaderMessage() . "\n\n" .
+                "*** Call expected $expectedCallCount time/s: ***\n" .
+                $this->generateMessage( $expectedMethodCall );
+
+        $message = $expectedMessage . "\n" .
+                "*** Actual calls :- ***\n" . $actualCallMessage;
+
+        $this->raiseAssertionError(
+            $message,
+            $expectedMethodCall
+        );
     }
 
 
-    private function raiseAssertionError( $baseMessage, MethodCall $expectedMethodCal  ) {
+    private function generateMessage( MethodCall $methodCall ) {
+        $methodSignature = get_class( $methodCall->getClass() ) . '->' . $methodCall->getMethod() . "()\n";
+        foreach ( $methodCall->getArguments() as $index => $argument ) {
+            $methodSignature .= '   arg[' . $index . '] = ' . var_export( $argument, true ) . "\n";
+        }
+
+        return $methodSignature;
+    }
+
+
+    private function generateHeaderMessage() {
+        $debug_backtrace = debug_backtrace();
+
+        $testMethodName = $debug_backtrace[ 4 ][ 'function' ];
+        $file           = $debug_backtrace[ 3 ][ 'file' ];
+        $line           = $debug_backtrace[ 3 ][ 'line' ];
+
+        return '*** Test:' . $testMethodName . " ***\n" .
+        $file . '(' . $line . ')';
+
+
+    }
+
+
+    private function raiseAssertionError( $baseMessage, MethodCall $expectedMethodCal ) {
         throw new \PHPUnit_Framework_AssertionFailedError( $baseMessage );
     }
 
