cc90d4c8d2263e2f4efa35565ced2d62ca34d76c
pbyrne84 <YourEmail>
2013-10-28 19:41:58 +0000

    make closure safe

--- lib/PHPMockito/Expectancy/TestClassCallBacktraceDetector.php
+++ lib/PHPMockito/Expectancy/TestClassCallBacktraceDetector.php
@@ -10,11 +10,28 @@ class TestClassCallBacktraceDetector {
     public function callWasDoneByTestClass( $testClass, array $debugBackTrace ) {
         $testCallInitialisationDepth = 1;
 
+        return $this->runCallAnalysis( $testClass, $debugBackTrace, $testCallInitialisationDepth );
+    }
+
+
+    /**
+     * @param stirng $testClass
+     * @param array  $debugBackTrace
+     * @param int    $testCallInitialisationDepth
+     *
+     * @return bool
+     * @throws \InvalidArgumentException
+     */
+    private function runCallAnalysis( $testClass, array $debugBackTrace, $testCallInitialisationDepth ) {
         if ( count( $debugBackTrace ) < $testCallInitialisationDepth + 1 ) {
             throw new \InvalidArgumentException( 'Backtrace cannot be empty' );
         }
 
         $parentCallDepth = $testCallInitialisationDepth;
+        if ( false !== strpos( $debugBackTrace[ $parentCallDepth ][ 'function' ], '{closure}' ) ) {
+            return $this->runCallAnalysis( $testClass, $debugBackTrace, ++$testCallInitialisationDepth );
+        }
+
         if ( !isset( $debugBackTrace[ $parentCallDepth ][ 'class' ] ) ) {
             throw new \InvalidArgumentException(
                 'class has not been set in backtrace, found ' . print_r( $debugBackTrace[ $parentCallDepth ], true )
